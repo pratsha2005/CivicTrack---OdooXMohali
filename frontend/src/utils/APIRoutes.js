020aca38c4e8574b420040330d62555b7e6c35db
Gourav Kumar <gouravpanchal1632@gmail.com>
2025-08-02 12:57:35 +0530

    Add routes to frontend

--- frontend/src/utils/APIRoutes.js
+++ frontend/src/utils/APIRoutes.js
@@ -1,4 +1,14 @@
-// const port = // env se
+const port = import.meta.env.VITE_BACKEND_URL; // from .env
 
 export const loginUserRoute = `${port}/api/v1/users/login`;
 export const registerUserRoute = `${port}/api/v1/users/register`;
+export const logoutUserRoute = `${port}/api/v1/users/logout`;
+export const refreshTokenRoute = `${port}/api/v1/users/refresh-token`;
+export const getCurrentUserRoute = `${port}/api/v1/users/profile`;
+export const changePasswordRoute = `${port}/api/v1/users/changepassword`;
+export const updateNameAndAvatarRoute = `${port}/api/v1/users/update-profile`;
+export const updateLocationRoute = `${port}/api/v1/users/update-location`;
+
+export const registerIssueRoute = `${port}/api/v1/issues/register-issue`;
+export const getAllIssuesRoute = `${port}/api/v1/issues/getAllIssues`;
+export const getNearbyIssuesRoute = (radius) => `${port}/api/v1/issues/getNearbyIssues/${radius}`;

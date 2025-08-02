90cc05eb938b175312266b518d657ce8abc570b7
Gourav Kumar <gouravpanchal1632@gmail.com>
2025-08-02 13:01:22 +0530

    Fix issue route

--- backend/src/routes/issues.route.js
+++ backend/src/routes/issues.route.js
@@ -1,20 +1,25 @@
-const port = import.meta.env.VITE_BACKEND_URL;
+import { Router } from "express";
+import { registerIssues, getAllIssues, getNearbyIssues, editIssueStatus,
+reportIssue, getIssuesByUserId, editIssue, deleteIssue
+} from "../controllers/issues.controller.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { upload } from "../middlewares/multer.middleware.js"; // 🆕
 
-export const loginUserRoute = `${port}/api/v1/users/login`;
-export const registerUserRoute = `${port}/api/v1/users/register`;
-export const logoutUserRoute = `${port}/api/v1/users/logout`;
-export const refreshTokenRoute = `${port}/api/v1/users/refresh-token`;
-export const changePasswordRoute = `${port}/api/v1/users/changepassword`;
-export const profileRoute = `${port}/api/v1/users/profile`;
-export const updateUserRoute = `${port}/api/v1/users/update`; // 👈 for name/avatar
-export const updateLocationRoute = `${port}/api/v1/users/update-location`;
+const router = Router();
 
-// Issue Routes
-export const registerIssueRoute = `${port}/api/v1/issues/register-issue`;
-export const getAllIssuesRoute = `${port}/api/v1/issues/getAllIssues`;
-export const getNearbyIssuesRoute = `${port}/api/v1/issues/getNearbyIssues`;
-export const editIssueStatusRoute = (issueId) => `${port}/api/v1/issues/editIssue/${issueId}`;
-export const editIssueRoute = (id) => `${port}/api/v1/issues/editIssue/${id}`;
-export const reportIssueRoute = (id) => `${port}/api/v1/issues/reportIssue/${id}`;
-export const getIssuesByUserIdRoute = `${port}/api/v1/issues/getIssueByUserId`;
-export const deleteIssueRoute = (id) => `${port}/api/v1/issues/deleteIssue/${id}`;
+router
+  .route("/register-issue")
+  .post(
+    verifyJWT,
+    upload.array("images", 5), 
+    registerIssues
+  );
+
+router.route("/getAllIssues").get(getAllIssues);
+router.route("/getNearbyIssues").get(verifyJWT, getNearbyIssues);
+router.route("/editIssue/:issueId").post(editIssueStatus)
+router.route("editIssue/:id").post(editIssue)
+router.route("/reportIssue/:id").post(reportIssue)
+router.route("/getIssueByUserId").get(verifyJWT, getIssuesByUserId)
+router.route("/deleteIssue/:id").post(deleteIssue)
+export default router;
